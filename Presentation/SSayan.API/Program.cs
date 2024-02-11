f83e507496608339a490baaae39c7f634d7c4092
Ferhan Abaci <ferhanabacii@gmail.com>
2024-02-12 01:07:46 +0300

    jwt token dogrulama islemi yapıldı

--- Presentation/SSayan.API/Program.cs
+++ Presentation/SSayan.API/Program.cs
@@ -1,5 +1,7 @@
 using SSayan.Persistence;
 using SSayan.Application;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("admin")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateAudience = true,// olu?turulacak token degerini  kimlerin/kullan?cag?n? hangi sitelerin kullan?lacag? degerdir. www.bilmemne.com
+            ValidateIssuer = true, // olu?turulacak token degerini kimin dag?tacag?n? ifade edecegimz aland?r. www.myapi.com
+            ValidateLifetime = true,// olu?turalacak token süresi
+            ValidateIssuerSigningKey = true, //  olu?turalacak token degerinin uygulamam?za ait oldugunu ifade edecek security key verisini dogrulanmas?d?r
+
+
+            ValidAudience = builder.Configuration["Token:Audience"],
+            ValidIssuer = builder.Configuration["Token:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"]))
+
+        };
+    });
 
 var app = builder.Build();
 
